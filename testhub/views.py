63a128eee0b28795843715d15917c8375d24a3db
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-09-29 02:17:55 -0700

    change the index view, added a view that return json format and a view that query the json format view

--- testhub/views.py
+++ testhub/views.py
@@ -1,19 +1,38 @@
-from django.shortcuts import render, redirect
-from . models import Question, Answer
+from django.shortcuts import render
+from .models import Question, Answer
+from django.http import JsonResponse
+import requests
+
+
 # Create your views here.
-
-
 def index(request):
-    """
-    view function for our site's homepage
-    """
-    # genrate count information for questions
-    queryset = Question.objects.all()
-    # ans = Answer.objects.all()
-    # context = {
-    #    'queryset': queryset,
-    #    'options': ans
-    # }
+    """View function for home page"""
+    
+    
+    return (render(request, 'index.html'))
 
-    # render html template index.html with context data
-    return render(request, 'index.html', {'questions': queryset})
+
+# save the questions and answerns in json format
+def get_questions_json(request):
+    questions = Question.objects.all()
+    data = []
+    for question in questions:
+        answers = Answer.objects.filter(question=question)
+        question_data = {
+            'question': question.text,
+            'answers': [{'text': answer.text} for answer in answers]
+        }
+        data.append(question_data)
+        
+    return  JsonResponse({'data': data}) 
+
+# loop throught the json data
+def display_quiz(request):
+    response = requests.get('http://localhost:7000/testhub/get_questions_json')
+    
+    if response.status_code == 200:
+        data = response.json()['data']
+        return (render(request, 'quiz.html', {'data': data}))
+    
+    else:
+        return JsonResponse({'Error': 'failed to fetch quiz data'}, status=500)

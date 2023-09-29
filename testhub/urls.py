106614150e30b93cd7b6faf5be9f257b2c2b8f5d
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-09-29 02:16:32 -0700

    added urls to the methods defined in the views.py

--- testhub/urls.py
+++ testhub/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('get_questions_json/', views.get_questions_json, name='get_questions_json'),
+    path('display_quiz/', views.display_quiz, name='display_quiz'),
 ]

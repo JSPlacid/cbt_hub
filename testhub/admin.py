232c4f34550f1b8e25efa0bd5cd78804ebb7a2ca
JSPlacid <jspacific777@gmail.com>
2023-10-01 11:07:14 +0100

    created a CRUD view for permitted users to manipulate quiz questions

--- testhub/admin.py
+++ testhub/admin.py
@@ -4,13 +4,20 @@ from .models import Question, Answer
 # Register your models here.
 
 
-@admin.register(Answer)
-class AnswerAdmin(admin.ModelAdmin):
-    """class for answer model"""
-    list_display = ('text', 'question', 'is_correct')
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    extra = 0
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     """class for question model"""
     list_display = ['text']
+
+    inlines = [AnswerInline]
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    """class for answer model"""
+    list_display = ('text', 'question', 'is_correct')
--- testhub/views.py
+++ testhub/views.py
@@ -1,3 +1,7 @@
+from testhub.models import Question
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import render, redirect
 from . models import Question, Answer
 # Create your views here.
@@ -17,3 +21,23 @@ def index(request):
 
     # render html template index.html with context data
     return render(request, 'index.html', {'questions': queryset})
+
+
+# view func for author crud
+
+
+class QuestionCreate(PermissionRequiredMixin, CreateView):
+    model = Question
+    fields = [
+        'text'
+    ]
+
+
+class QuestionUpdate(PermissionRequiredMixin, CreateView):
+    model = Question
+    fields = '__all__'
+
+
+class QuestionCreate(PermissionRequiredMixin, CreateView):
+    model = Question
+    success_url = reverse_lazy('questions')

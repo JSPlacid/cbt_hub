cf96d24e503018833a32981fd3473c4356011617
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-10-01 01:18:38 -0700

    added methods to the view

--- testhub/views.py
+++ testhub/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question, Answer
-from django.http import JsonResponse
-import requests
 
 
 # Create your views here.
@@ -11,29 +9,67 @@ def index(request):
     return (render(request, 'index.html'))
 
 
-# save the questions and answerns in json format
-def get_questions_json(request):
-    questions = Question.objects.all()
-    data = []
-    for question in questions:
-        answers = Answer.objects.filter(question=question)
-        question_data = {
-            'question': question.text,
-            'answers': [{'text': answer.text} for answer in answers]
+def get_questions(request, question_id):
+    """Method that get the questions from the data
+        store the users response in a session
+    """
+    
+    question = get_object_or_404(Question, pk=question_id)
+    answers = Answer.objects.filter(question=question)
+    
+    #display nextbutton based on the question
+    nextQuestion =Question.objects.filter(id__gt=question.id).order_by('id').first()
+    #display prev button based on the question
+    prevQuestion =Question.objects.filter(id__lt=question.id).order_by('-id').first()
+    
+    
+    if request.method == 'POST':
+        selected_answer_id = request.POST.get('selected_answer')
+        selected_answer = get_object_or_404(Answer, pk=selected_answer_id)
+        is_correct = selected_answer.is_correct
+        
+        #check if 'user_responses' not in session
+        if 'user_responses' not in request.session:
+            requestion.session['user_responses'] = {}
+        
+        request.session['user_responses'][question_id] = {
+            'selected_answer_id': selected_answer_id,
+            'is_correct': is_correct
         }
-        data.append(question_data)
-
-    return JsonResponse({'data': data})
-
-# loop throught the json data
-
-
-def display_quiz(request):
-    response = requests.get('http://localhost:7000/testhub/get_questions_json')
-
-    if response.status_code == 200:
-        data = response.json()['data']
-        return (render(request, 'quiz.html', {'data': data}))
-
-    else:
-        return JsonResponse({'Error': 'failed to fetch quiz data'}, status=500)
+        
+        
+        if nextQuestion:
+            return (redirect('get_questions_json', question_id=nextQuestion.id))
+        else:
+            
+            return (render("Quiz completed"))
+        
+        
+    
+    return (render(request, 'quiz.html', {'question': question,
+                                          'answers': answers,
+                                          'prevQuestion': prevQuestion,
+                                          'nextQuestion': nextQuestion}))
+    
+    
+    
+    
+def review_quiz(request):
+        """Method that display the quiz result"""
+        
+        #Get the user responses from the session
+        user_responses = request.session.get('user_responses', {})
+        
+        
+        total_score = 0
+        for question_id, response_data in user_responses.items():
+            is_correct = response.info.get('is_correct', False)
+            if is_correct:
+                total_score += 1
+    
+        return (render(request, 'review.html',
+                   {
+                       'user_responses': user_responses,
+                       'total_score': total_score,
+                   }))
+        
